ca5c3eb37e51e39a9dd589135f04a80bf4b9e67b
munlikaCh <munlika_ch@cmu.ac.th>
2021-07-23 10:38:42 +0700

    1.11

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
+import EventDetails from '../views/EventDetails.vue'
+
 const routes = [
   {
     path: '/',
     name: 'EventList',
     component: EventList
-  },
-  {
+  },{
     path: '/about',
     name: 'About',
     component: About
+  },{
+    path: '/event/:id',
+    name: 'EventDetails',
+    component: EventDetails,
+    // props: true
   }
 ]
 
